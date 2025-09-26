239c00891cf0c5df48bd15bdc4d0c2a5e7b3e2c1
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2025-09-26 00:07:38 -0400

    minor edits

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-request = "GET /index.html HTTP/1.1 \r\nHost: localhost\r\n\r\n"
+request = "GET /indexfake.html HTTP/1.1 \r\nHost: localhost\r\n\r\n"
 clientSocket.send(request.encode())
 response = clientSocket.recv(1024)
 print ('From Server:', response.decode())
